b8ea457cd5f977ac44f2ceb4affaa5c638fd5dcc
Michael LeGrange <mlegrange15@att.net>
2018-10-15 07:39:52 -0500

    added home and saved components

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Home.css";
+import { Container } from "reactstrap";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <Container>
+        <h1>Home Component</h1>{" "}
+      </Container>
+    );
+  }
+}
+
+export default Home;
---
+++ client/src/components/Saved/Saved.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Saved.css";
+import { Container } from "reactstrap";
+
+class Saved extends Component {
+  state = {};
+  render() {
+    return (
+      <Container>
+        <h1>Saved Component</h1>
+      </Container>
+    );
+  }
+}
+
+export default Saved;
